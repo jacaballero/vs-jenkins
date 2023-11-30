151c2d7bd209aa2299261d01a4babe2016210823
jacaballero <juanantonio.caballero@uca.es>
2023-11-30 18:24:39 +0100

    actualizo archivo pdf-build.sh

--- marp/pdf-build.sh
+++ marp/pdf-build.sh
@@ -1,3 +1,4 @@
 #marp --allow-local-files --config-file ./marp/marp-engine.js --pdf $1.md -o pdf/$1.pdf --pdf-outlines --pdf-outlines.pages=false --pdf-outlines.headings=true
-npx @marp-team/marp-cli@latest --allow-local-files --config-file ./marp/marp-engine.js --pdf $1.md -o pdf/$1.pdf --pdf-outlines --pdf-outlines.pages=false --pdf-outlines.headings=true
+#npx @marp-team/marp-cli@latest --allow-local-files --config-file ./marp/marp-engine.js --pdf $1.md -o pdf/$1.pdf --pdf-outlines --pdf-outlines.pages=false --pdf-outlines.headings=true
 
+npx @marp-team/marp-cli --allow-local-files --config-file ./marp/marp-engine.js --pdf cultura.md -o pdf/cultura.pdf --pdf-outlines --pdf-outlines.pages=false --pdf-outlines.headings=true
